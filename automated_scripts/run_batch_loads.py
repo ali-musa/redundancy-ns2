3350508ddb8510611917c0cf3cd925c4a14d6db3
Ali Musa Iftikhar <musa@eecs.tufts.edu>
2016-07-16 11:20:09 -0400

    Update run_batch_loads.py

--- automated_scripts/run_batch_loads.py
+++ automated_scripts/run_batch_loads.py
@@ -13,7 +13,7 @@ for load in [1,10,20,30,40,50,60,70,80,90,95]:
 	print load
 
 	# spawn job
-	os.system("sbatch --time=2:00:00 llvr.tcl "+str(load)+" "+sys.argv[1]) #todo: dynamically set max time for faster job placement
+	os.system("sbatch -mem=16000 --time=2:00:00 llvr.tcl "+str(load)+" "+sys.argv[1]) #todo: dynamically set max time for faster job placement
 	os.system("wait")
 	time.sleep(0.25)
 print "done"
